6af12914fca73c99c4ba6a3a7f7a55372cf44cda
Hunter13004 <sharkhunter13x@gmail.com>
2024-10-18 02:15:52 -0400

    refactored code
    😏

--- fuckinshit.py
+++ fuckinshit.py
@@ -1,123 +1,135 @@
 import discord
+from cv2 import Mat
 from discord import app_commands
-from discord.ext import commands
 import cv2
 import numpy as np
 import requests
 from io import BytesIO
 from PIL import Image
 import pytesseract
+from typing import Sequence
 import os
 
+bot_token: str = 'token'
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 botIntents = discord.Intents.default()
 botClient = discord.Client(intents=botIntents)
 botCommandTree = app_commands.CommandTree(botClient)
-maps = [
+map_name_list = [
     "Antarctic Peninsula", "Busan", "Ilios", "Lijiang Tower", "Nepal", "Oasis", "Samoa", "Circuit Royal",
     "Dorado", "Havana", "Junkertown", "Rialto", "Route 66", "Shambali Monastery", "Watchpoint: Gibraltar",
     "New Junk City", "Suravasa", "Blizzard World", "Eichenwalde", "Hollywood", "King's Row", "Midtown",
-    "Numbani", "Paraiso", "Colosseo", "Esperanca", "New Queen Street", "Runasapi", "Hanaoka", "Throne of Anubis"
+    "Numbani", "Paraíso", "Colosseo", "Esperança", "New Queen Street", "Runasapi", "Hanaoka", "Throne of Anubis"
 ]
 
-def saveCutImg(img, i):
-    imgP = f"cropped_row_{i}.png"
-    cv2.imwrite(imgP, img)
-    print(f"Saved cropped row as {imgP}")
 
-def findBox(mask):
+def save_cropped_image(img: Mat | np.ndarray[np.any, np.dtype] | np.ndarray, row_number: int) -> None:
+    file_name = f"cropped_row_{row_number}.png"
+    cv2.imwrite(file_name, img)
+    print(f"Saved cropped row as {file_name}")
+
+
+def find_box(mask: Mat | np.ndarray) -> Sequence[int] or None:
     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     if contours:
         foo = max(contours, key=cv2.contourArea)
         return cv2.boundingRect(foo)
     return None
 
-def findOrangeP(mask):
+
+def find_orange_percent(mask: Mat | np.ndarray) -> int:
     return (cv2.countNonZero(mask) / mask.size) * 100
 
-def processImg(url):
+
+def process_image(url: str) -> tuple[list[str], list[str]]:
     response = requests.get(url)
-    print("got img")
-    img = Image.open(BytesIO(response.content))
-    img = img.resize((1920, 1080), Image.Resampling.LANCZOS)
-    print("resized img.")
-    image = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
-    print("converted img to cv.")
+    print("got source image")
+
+    source_image = Image.open(BytesIO(response.content))
+    source_image = source_image.resize((1920, 1080), Image.Resampling.LANCZOS)
+    print("resized source image.")
+    image = cv2.cvtColor(np.array(source_image), cv2.COLOR_RGB2BGR) # TODO np.array DOES NOT TAKE IMAGEFILE AS A TYPE SEE DOC: https://numpy.org/doc/stable/reference/generated/numpy.array.html
+    print("converted image colorspace to cv.")
+
     template = cv2.imread('share.png')
     if template is None:
         return [], []
 
     print("got share location temp ")
-    imageGrey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    templateGrey = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
-    result = cv2.matchTemplate(imageGrey, templateGrey, cv2.TM_CCOEFF_NORMED)
-    iconH, iconW = templateGrey.shape
+
+    image_grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    template_grey = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+    result = cv2.matchTemplate(image_grey, template_grey, cv2.TM_CCOEFF_NORMED)
+    icon_height, icon_width = template_grey.shape
     locations = np.where(result >= 0.8)
     print(f"found {len(locations[0])} locations")
     print(f"locations: {locations}")
+
     if len(locations[0]) == 0:
-        print("couldnt find share icaon in img")
+        print("couldn't find share icon in image")
         return [], []
 
-    lowerO, upperO = np.array([5, 100, 100]), np.array([20, 255, 255])
-    relX, relY, relW, relH, count = None, None, None, None, 1
-    rowT, codeT = [], []
-
+    lower_orange, upper_orange = np.array([5, 100, 100]), np.array([20, 255, 255])
+    relative_x, relative_y, relative_width, relative_height, count = None, None, None, None, 1
+    row_text_list, code_text_list = [], []
 
     for pt in zip(*locations[::-1]):
         print(pt)
-        row = image[max(0, pt[1] - 15):min(image.shape[0], pt[1] + iconH + 25), :]
-        saveCutImg(row, count)
-        hsvR = cv2.cvtColor(row, cv2.COLOR_BGR2HSV)
-        maskO = cv2.inRange(hsvR, lowerO, upperO)
+        row = image[max(0, pt[1] - 15):min(image.shape[0], pt[1] + icon_height + 25), :]
+        save_cropped_image(row, count)
+        hue_saturation_value_row = cv2.cvtColor(row, cv2.COLOR_BGR2HSV)
+        mask_orange = cv2.inRange(hue_saturation_value_row, lower_orange, upper_orange)
         print(f"row {count}")
 
         if count == 1:
-            x, y, w, h = findBox(maskO)
-            print(f"cubox for row: {(x, y, w, h)}")
+            x, y, w, h = find_box(mask_orange)
+            print(f"cut box for row: {(x, y, w, h)}")
             if x is not None:
-                relX, relY, relW, relH = x / iconW, y / iconH, w / iconW, h / iconH
-                boxO = row[y:y + h, x:x + w]
-                codeText = pytesseract.image_to_string(boxO)
-                codeT.append(codeText)
-                print(f"code text: {codeText}")
+                relative_x, relative_y, relative_width, relative_height = x / icon_width, y / icon_height, w / icon_width, h / icon_height
+                box_orange = row[y:y + h, x:x + w]
+                code_text = pytesseract.image_to_string(box_orange)
+                code_text_list.append(code_text)
+                print(f"code text: {code_text}")
 
         else:
-            if relX is not None:
-                x, y, w, h = int(relX * iconW), int(relY * iconH), int(relW * iconW), int(relH * iconH)
+            if relative_x is not None:
+                x, y, w, h = int(relative_x * icon_width), int(relative_y * icon_height), int(relative_width * icon_width), int(relative_height * icon_height)
                 cropped = row[y:y + h, x:x + w]
-                orangeP = findOrangeP(
-                    cv2.inRange(cv2.cvtColor(cropped, cv2.COLOR_BGR2HSV), lowerO, upperO))
-                print(f"orangeP for {count}: {orangeP}%")
-                if orangeP >= 70:
-                    codeText = pytesseract.image_to_string(cropped)
-                    codeT.append(codeText)
-                    print(f"{count} code {codeText}")
+                orange_percent = find_orange_percent(
+                    cv2.inRange(cv2.cvtColor(cropped, cv2.COLOR_BGR2HSV), lower_orange, upper_orange))
+                print(f"orangeP for {count}: {orange_percent}%")
+                if orange_percent >= 70:
+                    code_text = pytesseract.image_to_string(cropped)
+                    code_text_list.append(code_text)
+                    print(f"{count} code {code_text}")
 
-        rowText = pytesseract.image_to_string(row)
-        rowT.append(rowText)
-        print(f"{count} text {rowText}")
+        row_text = pytesseract.image_to_string(row)
+        row_text_list.append(row_text)
+        print(f"{count} text {row_text}")
         count += 1
 
     print("done")
-    return rowT, codeT
+    return row_text_list, code_text_list
 
-def formatText(name, rowT, codeT):
-    foo = f"{name}\n"
-    bar = min(len(rowT), 7)
-    for i in range(bar, 0, -1):
-        foo += f"{rowT[i - 1]}: {codeT[i - 1]}\n"
+
+def format_text(title: str, row_text_list: list[str], code_text_list: list[str]) -> str:
+    formatted_text = f"{title}\n"
+    row_count = min(len(row_text_list), 7)
+    for i in range(row_count, 0, -1):
+        formatted_text += f"{row_text_list[i - 1]}: {code_text_list[i - 1]}\n"
     print("text formatted")
-    return foo
+    return formatted_text
 
-def checkMap(rowT):
-    for i in range(len(rowT)):
-        for map in maps:
-            if map.lower() in rowT[i].lower():
-                rowT[i] = map
-                print(f"'{map}'in {i + 1}.")
+
+def check_map(row_text_list: list[str]) -> list[str]:
+    for i in range(len(row_text_list)):
+        for map_name in map_name_list:
+            if map_name.lower() in row_text_list[i].lower():
+                row_text_list[i] = map_name
+                print(f"'{map_name}'in {i + 1}.")
                 break
-    return rowT
+    return row_text_list
+
 
 @botClient.event
 async def on_ready():
@@ -129,27 +141,28 @@ async def on_ready():
     except Exception as e:
         print(f"Failed to sync commands: {e}")
 
+
 @app_commands.allowed_installs(guilds=True, users=True)
 @app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
 @app_commands.user_install
 @botCommandTree.command(name="getcodes", description="Extracts replays from OW2 screenshot")
-async def processimage(interaction: discord.Interaction, name: str, number_of_replays: int, attachment: discord.Attachment):
+async def get_codes(interaction: discord.Interaction, name: str, attachment: discord.Attachment):
     try:
         await interaction.response.defer()
-        print(f"recived cmd getcodes, {name}, {attachment.filename}")
-        imgURL = attachment.url
-        rowT, codeT = processImg(imgURL)
-        for i, (rT, cT) in enumerate(zip(rowT, codeT), start=1):
+        print(f"received cmd getcodes, {name}, {attachment.filename}")
+        img_url = attachment.url
+        row_text_list, code_text_list = process_image(img_url)
+        for i, (rT, cT) in enumerate(zip(row_text_list, code_text_list), start=1):
             print(f"row {i}, text from tess: {rT}")
             print(f"code {i}, text from tess: {cT}")
-        filtered = [(row, code) for row, code in zip(rowT, codeT) if 'CUSTOM' in row.upper()]
-        rowT, codeT = zip(*filtered) if filtered else ([], [])
-        rowT = checkMap(list(rowT))
-        formatted = formatText(name, rowT, codeT)
+        filtered = [(row, code) for row, code in zip(row_text_list, code_text_list) if 'CUSTOM' in row.upper()]
+        row_text_list, code_text_list = zip(*filtered) if filtered else ([], [])
+        row_text_list = check_map(list(row_text_list))
+        formatted = format_text(name, row_text_list, code_text_list)
         await interaction.followup.send(f"```\n{formatted}\n```")
         print("sent")
     except Exception as e:
         await interaction.followup.send(f"error {str(e)}")
         print(f"err {e}")
 
-botClient.run('TOKEM')
+botClient.run(bot_token)
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-#code by kaitxk | kaixk.xyz
-#designed by hunter13004
+# code by kaitxk | kaixk.xyz
+# designed by hunter13004
 
 import discord
 from discord import app_commands
@@ -11,158 +11,169 @@ import re
 import asyncio
 from fuzzywuzzy import process
 
-#global vars
-oskey = 'apikey'
-osurl = 'https://api.ocr.space/parse/image'
+# global vars
+ocr_space_key = 'apikey'
+ocr_space_api_url = 'https://api.ocr.space/parse/image'
 
 botIntents = discord.Intents.default()
 botClient = discord.Client(intents=botIntents)
 botCommandTree = app_commands.CommandTree(botClient)
 
-def gimmecodes(fp, key=oskey):
-    with open(fp, 'rb') as img:
-        stuff = {
+
+def process_image_using_ocr_space(image: Image, key: str = ocr_space_key) -> str:
+    with open(image, 'rb') as img:
+        payload = {
             'apikey': key,
             'language': 'eng'
         }
-        hii = requests.post(osurl, files={'file': img}, data=stuff)
-        hello = hii.json()
-        if not hello.get('IsErroredOnProcessing', True):
-            return hello['ParsedResults'][0]['ParsedText']
+        response = requests.post(ocr_space_api_url, files={'file': img}, data=payload)
+        response_json = response.json()
+        if not response_json.get('IsErroredOnProcessing', True):
+            return response_json['ParsedResults'][0]['ParsedText']
         else:
-            return f"Error: {hello['ErrorMessage']}"
+            return f"Error: {response_json['ErrorMessage']}"
 
-def cleanup(txt):
-    foo = re.sub(r'[<•$]', '', txt)
-    foo = re.sub(r'\s*:\s*', ':', foo)
-    foo = re.sub(r'9k', '', foo)
-    return foo.strip()
 
-def processstuff(url, howmany):
-    hiii = requests.get(url)
-    img = Image.open(BytesIO(hiii.content))
-    img = img.resize((1920, 1080), Image.Resampling.LANCZOS)
-    snippyzones = [
+def cleanup_text(text: str) -> str:
+    cleaned_text = re.sub(r'[<•$]', '', text)
+    cleaned_text = re.sub(r'\s*:\s*', ':', cleaned_text)
+    cleaned_text = re.sub(r'9k', '', cleaned_text)
+    return cleaned_text.strip()
+
+
+def process_image(url: str, replay_count: int) -> list[list[str]]:
+    response = requests.get(url)
+    source_image = Image.open(BytesIO(response.content))
+    source_image = source_image.resize((1920, 1080), Image.Resampling.LANCZOS)
+    cropping_zones = [
         (430, 396, 487, 68),
         (916, 407, 105, 24),
         (1021, 396, 240, 68)
     ]
-    amtdown = [72, 73, 72]
-    rs = []
-    for rn in range(1, howmany + 1):
-        r = []
-        for tn, (x, y, w, h) in enumerate(snippyzones):
-            snip = (x, y, x + w, y + h)
-            circumcised = img.crop(snip)
-            temppath = f'temp_row{rn}_text{tn + 1}.png'
-            circumcised.save(temppath)
-            raw = gimmecodes(temppath)
-            goodtext = cleanup(raw) if raw else ""
-            r.append(goodtext)
-            snippyzones[tn] = (x, y + amtdown[tn], w, h)
-            os.remove(temppath)
-        if 'WORKSHOP CHAMBER' in r[0].upper():
+    crop_zone_increments = [72, 73, 72]
+    row_list = []
+    for row_number in range(1, replay_count + 1):
+        row = []
+        for col_number, (x, y, width, height) in enumerate(cropping_zones):
+            snip = (x, y, x + width, y + height)
+            cropped_image = source_image.crop(snip)
+            temporary_filename = f'temp_row{row_number}_text{col_number + 1}.png'
+            cropped_image.save(temporary_filename)
+            raw_text = process_image_using_ocr_space(temporary_filename)
+            cleaned_text = cleanup_text(raw_text) if raw_text else ""
+            row.append(cleaned_text)
+            cropping_zones[col_number] = (x, y + crop_zone_increments[col_number], width, height)
+            os.remove(temporary_filename)
+        if 'WORKSHOP CHAMBER' in row[0].upper():
             continue
-        if 'CUSTOM GAME' in r[2].upper():
-            rs.append([r[0], r[1]])
-    return rs
+        if 'CUSTOM GAME' in row[2].upper():
+            row_list.append([row[0], row[1]])
+    return row_list
 
-async def keepoutofway(url, rplys):
-    return await asyncio.to_thread(processstuff, url, rplys)
 
-def ifixit(foo):
-    chars = {"I": "1", "O": "0", "U": "V", "!": "1", "$": "S", "&": "8"}
-    bar = list(foo)
-    for i, item in enumerate(bar):
-        for key, value in chars.items():
+async def start_image_processing(url: str, replay_count: int) -> asyncio.Coroutine:
+    return await asyncio.to_thread(process_image, url, replay_count)
+
+
+def replay_code_autocorrect(raw_code_text: str) -> str:
+    char_replacement_dict = {"I": "1", "O": "0", "U": "V", "!": "1", "$": "S", "&": "8"}
+    raw_code_char_list = list(raw_code_text)
+    for i, item in enumerate(raw_code_char_list):
+        for key, value in char_replacement_dict.items():
             if item == key:
-                bar[i] = value
-    foobar = "".join(bar)
-    return foobar
+                raw_code_char_list[i] = value
+    corrected_code_text = "".join(raw_code_char_list)
+    return corrected_code_text
 
-def ifixitmap(dumb):
-    overwatch_maps  = [
-    "Antarctic Peninsula",
-    "Busan",
-    "Ilios",
-    "Lijiang Tower",
-    "Nepal",
-    "Oasis",
-    "Samoa",
-    "Circuit Royal",
-    "Dorado",
-    "Havana",
-    "Junkertown",
-    "Rialto",
-    "Route 66",
-    "Shambali Monastery",
-    "Watchpoint: Gibraltar",
-    "New Junk City",
-    "Suravasa",
-    "Blizzard World",
-    "Eichenwalde",
-    "Hollywood",
-    "King's Row",
-    "Midtown",
-    "Numbani",
-    "Paraiso",
-    "Colosseo",
-    "Esperanca",
-    "New Queen Street",
-    "Runasapi",
-    "Hanaoka",
-    "Throne of Anubis"
-]
-    sigma, s = process.extractOne(dumb, overwatch_maps)
-    if s > 70:
-        return sigma
+
+def map_name_autocorrect(map_name: str) -> str:
+    overwatch_maps = [
+        "Antarctic Peninsula",
+        "Busan",
+        "Ilios",
+        "Lijiang Tower",
+        "Nepal",
+        "Oasis",
+        "Samoa",
+        "Circuit Royal",
+        "Dorado",
+        "Havana",
+        "Junkertown",
+        "Rialto",
+        "Route 66",
+        "Shambali Monastery",
+        "Watchpoint: Gibraltar",
+        "New Junk City",
+        "Suravasa",
+        "Blizzard World",
+        "Eichenwalde",
+        "Hollywood",
+        "King's Row",
+        "Midtown",
+        "Numbani",
+        "Paraiso",
+        "Colosseo",
+        "Esperanca",
+        "New Queen Street",
+        "Runasapi",
+        "Hanaoka",
+        "Throne of Anubis"
+    ]
+    corrected_map_name, autocorrect_confidence = process.extractOne(map_name, overwatch_maps)
+    if autocorrect_confidence > 70:
+        return corrected_map_name
     else:
-        return dumb
+        return map_name
 
-def filterText(data):
-  lines = data.splitlines()
-  names = []
-  codes = []
 
-  for line in lines:
-      name, code = line.split(": ")
-      names.append(name)
-      filtered_code = ifixit(code)
-      codes.append(filtered_code)
+def filter_text(data: str) -> str:
+    lines = data.splitlines()
+    names = []
+    codes = []
 
-  merged_data = "\n".join([f"{name}: {code}" for name, code in zip(names, codes)])
-  return merged_data
+    for line in lines:
+        name, code = line.split(": ")
+        names.append(name)
+        filtered_code = replay_code_autocorrect(code)
+        codes.append(filtered_code)
 
-def format(name, rows):
-    r = f"{name}\n"
+    merged_data = "\n".join([f"{name}: {code}" for name, code in zip(names, codes)])
+    return merged_data
+
+
+def format_text(name: str, rows: list[list[str]]) -> str:
+    formatted_text = f"{name}\n"
     for row in rows:
-        r += f"{ifixitmap(row[0])}: {ifixit(row[1])}\n"
-    return r.strip()
+        formatted_text += f"{map_name_autocorrect(row[0])}: {replay_code_autocorrect(row[1])}\n"
+    return formatted_text.strip()
+
 
 @app_commands.allowed_installs(guilds=True, users=True)
 @app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
 @app_commands.user_install
 @botCommandTree.command(name="getcodes", description="Extracts replays from OW2 screenshot")
-async def getcodes(interaction: discord.Interaction, name: str, attachment: discord.Attachment):
+async def get_codes(interaction: discord.Interaction, name: str, attachment: discord.Attachment):
     await interaction.response.defer()
     url = attachment.url
     amt = 7
-    rows = await keepoutofway(url, amt)
-    finaltxt = format(name, rows)
-    await interaction.followup.send(f"\n{finaltxt}\n")
+    rows = await start_image_processing(url, amt)
+    final_text = format_text(name, rows)
+    await interaction.followup.send(f"\n{final_text}\n")
+
 
 @app_commands.allowed_installs(guilds=True, users=True)
 @app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
 @app_commands.user_install
 @botCommandTree.command(name="getcodesraw", description="Extracts replays from OW2 screenshot, shows them only to you")
-async def getcodesraw(interaction: discord.Interaction, attachment: discord.Attachment):
+async def get_codes_raw(interaction: discord.Interaction, attachment: discord.Attachment):
     await interaction.response.defer(ephemeral=True)
     url = attachment.url
     amt = 7
     name = ""
-    rows = await keepoutofway(url, amt)
-    finaltxt = format(name, rows)
-    await interaction.followup.send(f"\n{finaltxt}\n")
+    rows = await start_image_processing(url, amt)
+    final_text = format_text(name, rows)
+    await interaction.followup.send(f"\n{final_text}\n")
+
 
 @app_commands.allowed_installs(guilds=True, users=True)
 @app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
@@ -172,9 +183,11 @@ async def feedback(interaction: discord.Interaction):
     await interaction.response.defer()
     await interaction.followup.send("https://forms.gle/esXC72V6hGznYGmu9")
 
+
 @botClient.event
 async def on_ready():
     await botCommandTree.sync()
     print(f'logged in as {botClient.user}')
+
 
 botClient.run('token')
